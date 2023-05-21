0c22e14e292dc41f02e3bafe13d73e000a55ed62
Allyson Gonzales <Allysonmg21@gmail.com>
2023-05-21 16:29:47 -0700

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import Header from './components/Header';
+import Navigation from './components/Navigation';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
 import './App.css';
-
-function App() {
+//needs adjustments here
+const App= () =>{
+  const [currentSection, setCurrentSection] = useState('About');
+  const handleNavClick = (section) => {
+    setCurrentSection(section);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+      <Navigation currentSection={currentSection} handleNavClick={handleNavClick} />
+      <main>
+        {currentSection === 'about' && <About />}
+        {currentSection === 'portfolio' && <Portfolio />}
+        {currentSection === 'contact' && <Contact />}
+        {currentSection === 'resume' && <Resume />}
+      </main>
+      <footer>
+        {/* Footer content coming soon */}
+      </footer>
     </div>
   );
-}
+};
 
 export default App;
