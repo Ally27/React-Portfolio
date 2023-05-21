1f8e5710517d68f70a42e09b65daf192b07a3b23
Allyson Gonzales <Allysonmg21@gmail.com>
2023-05-21 16:30:09 -0700

    updated Header & Nav.js

---
+++ src/components/Navigation.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Navigation = ({ currentSection, handleNavClick }) => {
+  return (
+    <nav>
+      <ul>
+        <li className={currentSection === 'about' ? 'active' : ''}>
+          <a href="#about" onClick={() => handleNavClick('about')}>
+            About Me
+          </a>
+        </li>
+        <li className={currentSection === 'portfolio' ? 'active' : ''}>
+          <a href="#portfolio" onClick={() => handleNavClick('portfolio')}>
+            Portfolio
+          </a>
+        </li>
+        <li className={currentSection === 'contact' ? 'active' : ''}>
+          <a href="#contact" onClick={() => handleNavClick('contact')}>
+            Contact
+          </a>
+        </li>
+        <li className={currentSection === 'resume' ? 'active' : ''}>
+          <a href="#resume" onClick={() => handleNavClick('resume')}>
+            Resume
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
